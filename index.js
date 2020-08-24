b92bc0dcdff2c5712b3def328f4c66ab5704cf5b
S4NDIX <sandix390@gmail.com>
2020-08-23 21:41:14 -0500

    Added auth

--- index.js
+++ index.js
@@ -18,22 +18,9 @@ app.get("/api", (req, res) => {
   );
 });
 
-const allowedOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: process.env.ORIGIN_1,
   })
 );
 //Enable express
